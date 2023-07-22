cd0d8dfa75b4b7ec2d15726a8aeae9f13f1d688b
username <emailaddr>
2023-07-21 22:17:54 -0400

    basic interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [password, setPassword] = useState('')
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1>Generated password</h1>
+        <input type="text" value={password} />
+        <button type='button'>Copy password</button>
+        <br />
+        <input type="range" id="slider" max="40" min="4" step="1" value="13" class="ui-components-61pwxs"></input>
+        <br />
+        <label htmlFor="">Letters</label>
+        <input type="checkbox" name="letters" id="letters"/>
+
+        <label htmlFor="">Digits</label>
+        <input type="checkbox" name="digits" id="digits"/>
+
+        <label htmlFor="">Symbols</label>
+        <input type="checkbox" name="symbols" id="symbols"/>
+
+        <label htmlFor="">Special Characters</label>
+        <input type="checkbox" name="special" id="special"/>
     </>
   )
 }
